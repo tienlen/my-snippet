debb62ad6a5d8eb5b7ad90b814927c54f38b8db9
Yusuke Komatsu <tienlen042@gmail.com>
2013-01-18 14:02:32 +0900

    add php directory

---
+++ php/new_class.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * [class description]
+ *
+ * @category Library
+ */
+class class_name extends abstract_class {
+	/* Constants
+	-------------------------------*/
+	const PIYO = 'piyo';
+
+	/* Public Properties
+	-------------------------------*/
+	/* Protected Properties
+	-------------------------------*/
+	protected $_root = NULL;
+	protected static $_active = NULL;
+
+	/* Private Properties
+	-------------------------------*/
+	/* Magic Methods
+	-------------------------------*/
+	public function __construct() {
+		if(1 == 1) {
+			return TRUE;
+		}
+
+		return FALSE;
+	}
+
+	/* Public Methods
+	-------------------------------*/
+	/**
+	 * [foo description]
+	 * @param  [type] $bar [description(option)]
+	 * @return [type]      [description(option)]
+	 */
+	public function foo($bar) {
+
+	}
+
+	/* Protected Methods
+	-------------------------------*/
+	/* Private Methods
+	-------------------------------*/
+}
